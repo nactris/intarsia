73b85dfcd92ac8f88f9d65842c87be2dd2d4c7f1
nac <mindstorm1818@gmail.com>
2023-08-02 00:38:52 +0200

    added kahuna's purificattion

--- src/main/java/dot/funky/nacsworkshop/common/casting/Patterns.java
+++ src/main/java/dot/funky/nacsworkshop/common/casting/Patterns.java
@@ -3,10 +3,7 @@ package dot.funky.nacsworkshop.common.casting;
 import at.petrak.hexcasting.api.PatternRegistry;
 import at.petrak.hexcasting.api.spell.math.HexDir;
 import at.petrak.hexcasting.api.spell.math.HexPattern;
-import dot.funky.nacsworkshop.common.casting.spells.OpEqualType;
-import dot.funky.nacsworkshop.common.casting.spells.OpGetMedia;
-import dot.funky.nacsworkshop.common.casting.spells.OpReadSlot;
-import dot.funky.nacsworkshop.common.casting.spells.OpWriteSlot;
+import dot.funky.nacsworkshop.common.casting.spells.*;
 import net.minecraftforge.fml.ModList;
 
 import static at.petrak.hexcasting.api.HexAPI.modLoc;
@@ -19,6 +16,7 @@ public class Patterns {
         try {
             PatternRegistry.mapPattern(HexPattern.fromAngles("qe", HexDir.EAST), modLoc("typeequals"), OpEqualType.INSTANCE, false);
             PatternRegistry.mapPattern(HexPattern.fromAngles("aaedeaa", HexDir.SOUTH_WEST), modLoc("calculate_media"), OpGetMedia.INSTANCE, false);
+            PatternRegistry.mapPattern(HexPattern.fromAngles("dwd", HexDir.NORTH_WEST), modLoc("get_health"), OpHealth.INSTANCE, false);
 
             if (ModList.get().isLoaded("curios")) {
                 PatternRegistry.mapPattern(HexPattern.fromAngles("dedweeee", HexDir.SOUTH_EAST), modLoc("slot_write"), OpWriteSlot.INSTANCE, false);
---
+++ src/main/java/dot/funky/nacsworkshop/common/casting/spells/OpHealth.kt
@@ -0,0 +1,15 @@
+package dot.funky.nacsworkshop.common.casting.spells
+
+import at.petrak.hexcasting.api.spell.ConstMediaAction
+import at.petrak.hexcasting.api.spell.asActionResult
+import at.petrak.hexcasting.api.spell.casting.CastingContext
+import at.petrak.hexcasting.api.spell.getLivingEntityButNotArmorStand
+import at.petrak.hexcasting.api.spell.iota.Iota
+
+object OpHealth : ConstMediaAction {
+    override val argc = 1
+
+    override fun execute(args: List<Iota>, ctx: CastingContext): List<Iota> {
+        return args.getLivingEntityButNotArmorStand(0, argc).health.asActionResult
+    }
+}
