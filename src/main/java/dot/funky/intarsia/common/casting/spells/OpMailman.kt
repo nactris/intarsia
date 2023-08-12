725fe1f2040d7f23cdddf363a258bbf505fbded5
nactris <mindstorm1717@gmail.com>
2023-08-12 18:17:45 +0200

    fix sending

--- src/main/java/dot/funky/intarsia/common/casting/spells/OpMailman.kt
+++ src/main/java/dot/funky/intarsia/common/casting/spells/OpMailman.kt
@@ -26,7 +26,6 @@ object OpMailman : SpellAction {
     override val isGreat = true
 
     override fun execute(args: List<Iota>, ctx: CastingContext): Triple<RenderedSpell, Int, List<ParticleSpray>> {
-        Intarsia.LOGGER.info("start exe")
 
         val delta = args.getVec3(0, argc)
 
@@ -36,7 +35,6 @@ object OpMailman : SpellAction {
 
         val cost = MediaConstants.CRYSTAL_UNIT * stack.count / stack.maxStackSize
         val targetMiddlePos = ctx.caster.position().add(0.0, ctx.caster.eyeHeight / 2.0, 0.0)
-        Intarsia.LOGGER.info("done exe")
 
         return Triple(Spell(stack, delta), cost, listOf(ParticleSpray.cloud(targetMiddlePos, 2.0), ParticleSpray.burst(targetMiddlePos.add(delta), 2.0)))
 
@@ -46,17 +44,14 @@ object OpMailman : SpellAction {
 
     private class Spell(val stack: ItemStack, val delta: Vec3) : RenderedSpell {
         override fun cast(ctx: CastingContext) {
-            Intarsia.LOGGER.info("start cast")
 
             var list = listOf<ItemStack>()
 
             if (!stack.hasTag() || !stack.tag!!.contains("BlockEntityTag") || !stack.tag!!.getCompound("BlockEntityTag").contains("Items")) {
                 list = list.plus(stack)
-                Intarsia.LOGGER.info("item ")
 
 
             } else {
-                Intarsia.LOGGER.info("box ")
 
                 var itemlist = stack.tag!!.getCompound("BlockEntityTag").getList("Items", 10).copy()
                 for (i in 0..itemlist.size) {
@@ -72,7 +67,6 @@ object OpMailman : SpellAction {
 
 
             for (s in list) {
-                Intarsia.LOGGER.info("current: {} ", s)
 
                 if (block != null && block as BaseContainerBlockEntity != null) {
 
@@ -83,13 +77,11 @@ object OpMailman : SpellAction {
                             item = s.copy()
                             s.count = 0
                             block.setItem(i, item)
-                            Intarsia.LOGGER.info("place full")
 
                             break
                         } else if (item.`is`(s.item) && block.canPlaceItem(i, s) && item.hasTag() == s.hasTag() && item.tag == s.tag && item.count + s.count <= item.maxStackSize) {
                             item.count += s.count
                             s.count = 0
-                            Intarsia.LOGGER.info("increase full")
 
                             block.setItem(i, item)
                             break
@@ -97,25 +89,21 @@ object OpMailman : SpellAction {
                         } else if (item.`is`(s.item) && block.canPlaceItem(i, s) && item.hasTag() == s.hasTag() && item.tag == s.tag && item.count != item.maxStackSize) {
                             s.count -= item.maxStackSize - item.count
                             item.count = item.maxStackSize
-                            Intarsia.LOGGER.info("increase part")
 
                         }
                     }
 
                 }
                 if (s.count > 0) {
-                    Intarsia.LOGGER.info("start place")
 
                     val delta = ctx.caster.lookAngle.scale(0.5)
                     val entity = ItemEntity(ctx.world, pos.x, pos.y, pos.z, s.copy(), delta.x + (Math.random() - 0.5) * 0.1, delta.y + (Math.random() - 0.5) * 0.1, delta.z + (Math.random() - 0.5) * 0.1)
                     entity.setPickUpDelay(40)
                     s.count = 0
                     ctx.world.addWithUUID(entity)
-                    Intarsia.LOGGER.info("done place")
 
                 }
             }
-            Intarsia.LOGGER.info("done cast")
 
         }
     }
